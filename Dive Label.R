4a24ee83e01a6e9b7afc0de00531af5ad0e5ded8
Larry Zheng <larry.zheng@duke.edu>
2020-06-18 17:50:30 +0000

    Added write csv line

--- Dive Label.R
+++ Dive Label.R
@@ -7,3 +7,5 @@ setwd('/cloud/project/data/L1_edited/180753')
 zc084 <- read_csv("180753-Series.csv")
 
 merged <- cbind(zc084, dive_labels)
+
+write.csv(merged, "Zc084_dive_labels.csv", row.names=TRUE)
