000ecd3aecd6fc9ba771b2d9123a62cbe925da6d
amybubu <amybu373@gmail.com>
2020-06-18 21:07:48 +0000

    messed around with app + new app

--- BRS/app.R
+++ BRS/app.R
@@ -1,14 +1,13 @@
 library(shiny)
 
 data(Spring2019)
-data(summer)
-
+data(Summer2019)
 
 ui <- fluidPage(
     titlePanel("BRS Data for Summer & Spring 2019"),
     sidebarLayout(
         sidebarPanel(
-            selectInput("var",
+            checkboxGroupInput("var",
                         label = "Choose Summer or Spring 2019",
                         choices = c("Summer", "Spring"))
         ),
@@ -21,12 +20,32 @@ ui <- fluidPage(
 server <- function(input, output) {
     
     datasetInput <- reactive({
-        switch(input$dataset,
-               "Summer" = summer,
-               "Spring" = Spring2019)
+       # switch(input$dataset,
+              # "Summer" = Summer2019,
+              # "Spring" = Spring2019)
+        
+        if (input$distTable == "Summer2019") {
+            distTable <- Summer2019
+        } else {
+            distTable <- Spring2019
+        }
+        distTable
     })
     
-    output$distTable <- renderTable(Summer2019)
+    output$distTable <- renderTable({
+        #Summer2019
+        dataset <- datasetInput()
+    })
 }
+#server <- function(input, output){
+    #datasetInput <- reactive({
+        #df[df$Name == input$product, ]
+    #}) 
+    
+    #output$tsplot <- renderPlot({
+        #dataset <- datasetInput()
+        #plot(as.ts(dataset$Total_Sales, dataset$Date))
+   #})
+#}
 
 shinyApp(ui, server)
