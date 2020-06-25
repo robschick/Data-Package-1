03d46a32cb91da67698bd3b7a52b3b74967d8a95
Nick Kaney <66238429+nak27@users.noreply.github.com>
2020-06-25 13:59:25 -0700

    Sattag Shiny app from the viz team

---
+++ Sattag_location_shiny.R
@@ -0,0 +1,46 @@
+library(shiny)
+library(leaflet)
+library(sp)
+library(dplyr)
+
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
+
+# Define UI 
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("Preliminary Shiny App Locations Data"),
+    
+    
+    leafletOutput("mymap"),
+    p(),
+    
+)
+
+# Server Logic
+server <- function(input, output, session) {
+    
+    
+    
+    data <- read.csv("/cloud/project/June15/All_Locations.csv")
+    data$long <- as.numeric(data$Longitude)
+    data$lat <- as.numeric(data$Latitude)
+    
+    data <- data %>%
+        distinct(DeployID)
+    
+    
+    data.SP <- SpatialPointsDataFrame(data[, c(8,9)], data[, -c(8,9)])
+    
+    output$mymap <- renderLeaflet({
+        leaflet() %>%
+            addTiles() %>%
+            addMarkers(data = data.SP, lng = ~long, lat = ~lat)
+    })
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
