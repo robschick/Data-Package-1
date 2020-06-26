8d4b89660cee33303edceb4677884c617c1d6c1c
Larry Zheng <larry.zheng@duke.edu>
2020-06-26 05:55:25 +0000

    Created data package

---
+++ Test/BRSPackage/R/BRSPackage.R
@@ -0,0 +1,47 @@
+#' BRSPackage
+#' A data package for BRSPackage.
+#' @docType package
+#' @aliases BRSPackage-package
+#' @title Package Title
+#' @name BRSPackage
+#' @description A description of the data package
+#' @details Use \code{data(package='BRSPackage')$results[, 3]} to see a list of available data sets in this data package
+#'     and/or DataPackageR::load_all
+#' _datasets() to load them.
+#' @seealso
+#' \link{locations}
+NULL
+
+
+
+
+#' Detailed description of the data
+#' @name locations
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{spec_tbl_df} containing the following fields:
+#' \describe{
+#' \item{X1}{}
+#' \item{DeployID}{}
+#' \item{Ptt}{}
+#' \item{Instr}{}
+#' \item{Date}{}
+#' \item{Type}{}
+#' \item{Quality}{}
+#' \item{Latitude}{}
+#' \item{Longitude}{}
+#' \item{Error.radius}{}
+#' \item{Error.Semi.major.axis}{}
+#' \item{Error.Semi.minor.axis}{}
+#' \item{Error.Ellipse.orientation}{}
+#' \item{Offset}{}
+#' \item{Offset.orientation}{}
+#' \item{GPE.MSD}{}
+#' \item{GPE.U}{}
+#' \item{Count}{}
+#' \item{Comment}{}
+#' }
+#' @source The data comes from________________________.
+#' @seealso
+#' \link{BRSPackage}
+NULL
