76a77873f5224a8c8d1881fe1474eb98692eb019
Larry Zheng <larry.zheng@duke.edu>
2020-07-17 17:52:17 +0000

    removed comments, changed paths

--- package_script.R
+++ package_script.R
@@ -1,8 +1,6 @@
 library(DataPackageR)
 
-# In this example, I’ll tidy the cars data set by giving it more meaningful column names. 
-# The original data set has columns speed and dist. 
-# We’ll be more verbose and rename them to speed_mph and stopping_distance.
+# Tidying and packaging BRS data
 # 
 # An outline of the steps involved.
 # 1. Create a package directory structure. (DataPackageR::datapackage_skeleton())
@@ -55,81 +53,11 @@ datapackage_skeleton(name = "BRSPackage",
 
 
 
-# Series
-
-# 3. Add a data processing script. Edit your processing script (in Rmd file)
-#use_processing_script(file = "series.Rmd",
-                      #author = "Larry Zheng",
-                      #title = "Raw series.",
-                      #overwrite = TRUE)
-
-# 4. Let DataPackageR know about the data objects to store in the package.
-#use_data_object("series")
-
-
-# Build the package.
-#options("DataPackageR_interact" = FALSE)
-#package_build(packageName = '/cloud/project/Test/BRSPackage', install = FALSE)
-
-
-
-# Series Range
-
-# 3. Add a data processing script. Edit your processing script (in Rmd file)
-#use_processing_script(file = "series_range.Rmd",
-                      #author = "Larry Zheng",
-                      #title = "Raw series range.",
-                      #overwrite = TRUE)
-
-# 4. Let DataPackageR know about the data objects to store in the package.
-#use_data_object("series_range")
-
-
-# Build the package.
-#options("DataPackageR_interact" = FALSE)
-#package_build(packageName = '/cloud/project/Test/BRSPackage', install = FALSE)
-
-
-# CEE Metadata
-
-# 3. Add a data processing script. Edit your processing script (in Rmd file)
-#use_processing_script(file = "cee",
-                      #author = "Larry Zheng",
-                      #title = "Raw CEE.",
-                      #overwrite = TRUE)
-
-# 4. Let DataPackageR know about the data objects to store in the package.
-#use_data_object("cee")
-
-
-# Build the package.
-#options("DataPackageR_interact" = FALSE)
-#package_build(packageName = '/cloud/project/Test/BRSPackage', install = FALSE)
-
-
-
-# Locations Intersected with Deployment
-
-# 3. Add a data processing script. Edit your processing script (in Rmd file)
-#use_processing_script(file = "locations_intersect.Rmd",
-                      #author = "Larry Zheng",
-                      #title = "Locations Intersected with Deployment.",
-                      #overwrite = TRUE)
-
-# 4. Let DataPackageR know about the data objects to store in the package.
-#use_data_object("locations_intersect")
-
-
-# Build the package.
-#options("DataPackageR_interact" = FALSE)
-#package_build(packageName = '/cloud/project/Test/BRSPackage', install = FALSE)
-
-
-
 
 
 # function to process and include multiple dataframes in package
 package_function <- function(rmd_path, dataframe_to_store) {
+  # Data processing script (load in from rmd_path)
   use_processing_script(file = rmd_path,
                         overwrite = TRUE)
   
@@ -141,23 +69,21 @@ package_function <- function(rmd_path, dataframe_to_store) {
   package_build(packageName = paste(path, '/BRSPackage', sep = ""), install = FALSE)
 }
 
-# package_function("/cloud/project/data/raw_data/locations.Rmd", "locations")
-# package_function("/cloud/project/data/raw_data/series.Rmd", "series")
-# package_function("/cloud/project/data/raw_data/series_range.Rmd", "series_range")
-# package_function("/cloud/project/data/raw_data/locations_intersect.Rmd", "locations_intersect")
-# package_function("/cloud/project/data/raw_data/cee.Rmd", "cee")
 
+# paths of Rmd files used to process
 paths <- c("/cloud/project/data/raw_data/locations.Rmd",
            "/cloud/project/data/raw_data/series.Rmd", 
            "/cloud/project/data/raw_data/series_range.Rmd",
            "/cloud/project/data/raw_data/locations_intersect.Rmd",
            "/cloud/project/data/raw_data/cee.Rmd")
+# names of dataframes to store
 dataframes <- c("locations",
                 "series",
                 "series_range",
                 "locations_intersect",
                 "cee") 
 
+# Call function for each Rmd and each dataframe
 for (i in length(paths)) {
   package_function(paths[i], dataframes[i])
 }
@@ -166,11 +92,10 @@ for (i in length(paths)) {
 # Edit the data set documentation: edit the documentation.R file under data-raw
 # ensure we run document() from the DataPackageR namespace and not document() from roxygen or devtools.
 #package_path <- file.path(tempdir(),"TidyCars"))
-#DataPackageR::document(package_path)
+DataPackageR::document(path)
 
 
 
-# Iterate: add more data sets (make into function?)
 
 # Final build: rebuild the package one last time.
 #package_build(file.path(tempdir(),"Tidycars"))
