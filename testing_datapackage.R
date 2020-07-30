7b41426cb40cbbcd19b32e4815268d23562cd995
Larry Zheng <larry.zheng@duke.edu>
2020-07-30 17:42:16 +0000

    rebuilt package

--- testing_datapackage.R
+++ testing_datapackage.R
@@ -4,4 +4,4 @@ library(devtools)
 # Testing the use of the BRS data package
 
 # Install data package
-devtools::install_github("larryzheng0/BRSPackage")
+devtools::install_github("larryzheng0/PackageFolder/BRSPackage")
